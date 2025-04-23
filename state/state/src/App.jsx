fe24e1e9436dac860b3712117cbc9a5f5415b702
yahya-mohamoud <yahyamohamoud625@gmail.com.com>
2025-04-23 22:12:47 +0300

    created a counter using react states

---
+++ state/state/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export default function App () {
+  const [count, setCount] = useState(0)
+ 
+  return  (
+    <div className="btn" style={{display: 'flex', alignItems: 'center', gap: '10px'}}>
+        <button onClick={() => {setCount(count - 1)}} style={{height: '30px'}}>minus me</button>
+        <h1>{count}</h1>
+        <button onClick={() => {setCount(count + 1)}} style={{height: '30px'}}>add me</button>
+    </div>
+  )
+}
